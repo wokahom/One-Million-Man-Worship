f5662ec33db9870dc3f9a50ac62eb63123402eb3
Zerubbabel CNJ <wokahom@gmail.com>
2025-12-14 23:18:53 +0100

    UPDATE

--- js/youtube_live.js
+++ js/youtube_live.js
@@ -1,5 +1,5 @@
 
-  const liveTime = new Date("2025-12-13T16:00:00").getTime(); // SET YOUR LIVE TIME
+  const liveTime = new Date("2025-12-19T16:00:00").getTime(); // SET YOUR LIVE TIME
 
   const countdownEl = document.getElementById("countdown");
   const liveBadge = document.getElementById("liveBadge");
@@ -24,7 +24,7 @@
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    countdownEl.innerHTML = `Live starts in ${hours}h ${minutes}m ${seconds}s`;
+    countdownEl.innerHTML = `We will be LIVE in the next  ${hours}h ${minutes}m ${seconds}s`;
   }
 
   const timer = setInterval(updateCountdown, 1000);
